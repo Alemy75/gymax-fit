03a421a4afbb31bf9e87492bc17285569ef577f9
Max Alexeev <ma.a.alekseev@labmedia.su>
2024-11-25 18:21:07 +0300

    add: helpers

--- frontend/src/pages/home/ui/home.tsx
+++ frontend/src/pages/home/ui/home.tsx
@@ -1,7 +1,12 @@
 import { FC } from "react";
+import "./index.scoped.scss";
 
 const Home: FC = () => {
-  return <div className="g-home"></div>;
+  return (
+    <div className="g-home container">
+      <h3 className="card pt-8 opacity-disabled">Упражнения</h3>
+    </div>
+  );
 };
 
 export default Home;
